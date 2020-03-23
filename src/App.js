76ec7f2b8082b8dac166ac4b35c2259243d27d60
Prasant <33533156+5trang3@users.noreply.github.com>
2020-03-23 14:52:09 -0700

    Add event handler for adding players

--- src/App.js
+++ src/App.js
@@ -10,10 +10,20 @@ class App extends React.Component {
       players: [],
     }
   }
+
+  // Event handler for adding players:
+  addPlayer = () => {
+    let players = [...this.state.players];
+    players.push({});
+    this.setState({
+      players: players
+    })
+  }
+
   render() {
     return(
       <div>
-        <Button variant='contained' color='primary' endIcon={<PersonAddIcon/>}>
+        <Button variant='contained' color='primary' endIcon={<PersonAddIcon/>} onClick={this.addPlayer}>
           Add Player
         </Button>
       </div>
