5bad80aeb9c288c73d5ac73994e34d7fddb3c717
Prasant <33533156+5trang3@users.noreply.github.com>
2020-03-24 14:15:59 -0700

    Add method to create PlayerColumn components and render them

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button'
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import PlayerColumn from './components/PlayerColumn'
+import Grid from '@material-ui/core/Grid';
 
 class App extends React.Component {
   constructor(props) {
@@ -20,12 +22,28 @@ class App extends React.Component {
     })
   }
 
+  // Function for creating player columns:
+  createPlayerColumns = (players) => {
+    const playerColumns = players.map(function(player, index) {
+      return (
+        <Grid item xs={3}>
+          <PlayerColumn player={ player }/>
+        </Grid>
+      )
+    })
+    return playerColumns
+  }
+
   render() {
+    const playerColumns = this.createPlayerColumns(this.state.players)
     return(
       <div>
         <Button variant='contained' color='primary' endIcon={<PersonAddIcon/>} onClick={this.addPlayer}>
           Add Player
         </Button>
+        <Grid container spacing={1}>
+          { playerColumns }
+        </Grid>
       </div>
     )
   }
