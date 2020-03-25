870a3fa1db356bf7fbf2a4f7e4e876e17822f884
Prasant <33533156+5trang3@users.noreply.github.com>
2020-03-25 12:40:13 -0700

    Create error alert if too many players are added

--- src/App.js
+++ src/App.js
@@ -4,22 +4,35 @@ import Button from '@material-ui/core/Button'
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import PlayerColumn from './components/PlayerColumn'
 import Grid from '@material-ui/core/Grid';
+import Alert from '@material-ui/lab/Alert';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       players: [],
+      alerts: {
+        tooManyPlayers: 0
+      }
     }
   }
 
   // Event handler for adding players:
   addPlayer = () => {
     let players = [...this.state.players];
-    players.push({});
-    this.setState({
-      players: players
-    })
+    if (players.length < 4) {
+      players.push({});
+      this.setState({
+        players: players
+      })
+    }
+    else {
+      let alerts = Object.assign({}, this.state.alerts);
+      alerts.tooManyPlayers = 1;
+      this.setState({
+        alerts: alerts
+      })
+    }
   }
 
   // Function for creating player columns:
@@ -34,10 +47,30 @@ class App extends React.Component {
     return playerColumns
   }
 
+  // Function for creating alerts:
+  createAlerts = (alerts) => {
+    if (alerts.tooManyPlayers) {
+      return (
+        <Alert severity="error" onClose={() => this.removeAlerts('tooManyPlayers')}>There can only be a maximum of four players</Alert>
+      )
+    }
+  }
+
+  // Function for removing alerts:
+  removeAlerts = (alertId) => {
+    let alerts = Object.assign({}, this.state.alerts);
+    alerts[alertId] = 0;
+    this.setState({
+      alerts: alerts
+    })
+  }
+
   render() {
     const playerColumns = this.createPlayerColumns(this.state.players)
+    const alerts = this.createAlerts(this.state.alerts)
     return(
       <div>
+        { alerts }
         <Button variant='contained' color='primary' endIcon={<PersonAddIcon/>} onClick={this.addPlayer}>
           Add Player
         </Button>
